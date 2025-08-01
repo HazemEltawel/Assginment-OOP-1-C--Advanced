5227800463d99e30e32298be2f8efd9682e799f4
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-01 11:26:25 +0300

    Add project files.

---
+++ Assginment-OOP-1-C#-Advanced/implements.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assginment_OOP_1_C__Advanced
+{
+    public class implements
+    {
+         public static void OptimizedBubbleSort (int[] arr)
+        { 
+            int n = arr.Length;
+            bool swapp;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapp = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapp = true;
+
+                    }
+                }
+                if (!swapp)
+                {
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Experiance-01-C#-Advanced/Program.cs
@@ -0,0 +1,19 @@
+﻿using Assginment_OOP_1_C__Advanced;
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 5, 1, 4, 2, 3, 9, 6, 7, 8 };
+
+        implements.OptimizedBubbleSort( arr);
+        Console.WriteLine("Sorted array: ");
+        Console.WriteLine(string.Join(" ", arr));
+
+
+
+
+
+
+    }
+}
